b59809ff641d4a20b1664bd21d4fc9d160b05d8a
javier jimenez <javierrcc522@gmail.com>
2017-08-18 10:01:29 -0700

    a funtion to the back end to try to get all the numbers divisible by 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
+function pingpong() {
+  var resultNumber = [];
+  for( var i=0; i < arguments.length; i++){
+    if(i % 3 === 0)
+    resultNumber.push("ping")
+  };
+};
+
+
+
+
+
+
+
 $(document).ready(function(){
   $("form#project3").submit(function(event) {
     event.preventDefault();
-
     var userNumber = $("input#userInput").val();
+
   });
 });
