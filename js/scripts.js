7fb76c1e8d50974c82a2b3e3331da4961bfda36f
javier jimenez <javierrcc522@gmail.com>
2017-08-18 12:55:15 -0700

    add the array back beacuse i coulnt make it work without it

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,25 @@
 
 
 
-function pingpongMaster(num) {
-  var resultNumber = "";
-  for(var i=1; i<=num; i++){
-    if((i%15) === 0)
-    resultNumber += "ping-pong";
-  } else if((i%5) === 0) {
-      resultNumber += "pong"
+function pingpongMaster(numeros) {
+  var resultNumber = [];
+  for( var i = 1; i <= numeros; i++){
+    if(i%15 === 0)
+    resultNumber.push("ping-pong")
+    alert("ping-pong works")
+  } else if(i%5 === 0) {
+      resultNumber.push("pong")
       alert("ping-pong works1")
-  } else if ((i%3)=== 0) {
-      resultNumber += "ping"
+  } else if (i%3 === 0) {
+      resultNumber.push("ping")
       alert("ping-pong works2")
 
   } else {
-    resultNumber += i;
+    resultNumber.push(i);
+    return resultNumber;
   };
 };
-return resultNumber;
 
-};
 
 
 
@@ -31,9 +31,7 @@ $(document).ready(function(){
   $("form#project3").submit(function(event) {
     event.preventDefault();
     var userNumber = parseInt($("input#userInput").val());
-    var result = pingpongMaster(userNumber)
-    $("#userResult").append(pingpongMaster);
-    // var resultNumber = pingpongMaster()
-    // $("#userResult").append()
+    var resultNumber = pingpongMaster()
+    $("#userResult").append()
   });
 });
